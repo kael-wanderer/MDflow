a34a408e88f7eefabd02ca327484f5b1ff5d66ba
kael-wanderer <cong.buithanh@gmail.com>
2026-06-19 13:56:18 +0700

    chore: add trash crate, opener + clipboard plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,6 +13,8 @@ fn set_soft_wrap(app: tauri::AppHandle, on: bool) {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_opener::init())
         // Updater plugin is installed (dependency + capability) but dormant in M1.
         // M2 registers `tauri_plugin_updater` here and adds the `plugins.updater`
         // config (endpoints + pubkey).
